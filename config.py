8ff9a5d4347986689fa59dccbc50c61f81a356e5
Dula <anasahmad1234567890123@gmail.com>
2025-05-11 09:20:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
 
     # For Flask-MySQLdb
-    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "mysql.learnieverse.onrender.com"  # Use the correct Render MySQL host
+    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "mysql.railway.internal"  # Internal Railway MySQL host
     MYSQL_USER = os.environ.get("MYSQL_USER") or "root"
     MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD") or "zMaHYKnXXPNKehOeLulDVXYKJIFfvcOZ"
     MYSQL_DB = os.environ.get("MYSQL_DB") or "learnieverse_db"
