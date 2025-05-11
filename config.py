c167b068697c792d0e4c929f7a71978a1eef1c3a
Dula <anasahmad1234567890123@gmail.com>
2025-05-11 09:15:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
 
     # For Flask-MySQLdb
-    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "tramway.proxy.rlwy.net"
+    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "mysql.learnieverse.onrender.com"  # Use the correct Render MySQL host
     MYSQL_USER = os.environ.get("MYSQL_USER") or "root"
     MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD") or "zMaHYKnXXPNKehOeLulDVXYKJIFfvcOZ"
     MYSQL_DB = os.environ.get("MYSQL_DB") or "learnieverse_db"
-    MYSQL_PORT = int(os.environ.get("MYSQL_PORT") or 34169)
+    MYSQL_PORT = int(os.environ.get("MYSQL_PORT") or 3306)  # Default MySQL port
